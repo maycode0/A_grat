08d0f8c83c8b1e15195fd74965729690f20d9fc2
“maycode0” <2200453348@qq.com>
2025-12-26 17:19:44 +0800

    reset sparsity_lambda

--- sst2.sh
+++ sst2.sh
@@ -9,12 +9,13 @@ python run_guide.py \
     --dev_path data/$dataset/$dataset-validation.json \
     --test_path data/$dataset/$dataset-test.json \
     --max_length 256 \
+    --min_length 4 \
     --save_path results/$dataset/ \
     --save_name guide_st \
     --fix_embedding \
     --lr 0.0001 \
     --sparsity 0.15 \
-    --sparsity_lambda 10 \
+    --sparsity_lambda 5 \
     --continuity_lambda 10 \
     --guide_lambda 10 \
     --guide_decay 1e-5 \
